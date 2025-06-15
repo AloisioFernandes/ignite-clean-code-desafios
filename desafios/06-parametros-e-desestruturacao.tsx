23a49bf668d7d07fb5807c1647e53be7f8a46c23
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-06-15 17:03:32 -0300

    refactor: restructure function parameters for clarity and consistency.

--- desafios/06-parametros-e-desestruturacao.tsx
+++ desafios/06-parametros-e-desestruturacao.tsx
@@ -1,5 +1,8 @@
 function updateUserRoute({ body, params }) {
-  updateUserController({ data: body, params });
+  const { name, email, password } = body;
+  const { id } = params;
+
+  updateUserController({ data: { name, email, password }, params: { id } });
 }
 
 function updateUserController({ data, params }) {

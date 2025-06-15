5ca33a9bed3b2514bd2b79f8aa49dfda3ca0ef5e
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-06-15 17:56:42 -0300

    refactor: improve getFirstFiveRatings function for clarity and consistency.

--- desafios/09-syntatic-sugars.ts
+++ desafios/09-syntatic-sugars.ts
@@ -1,9 +1,17 @@
 function getFirstFiveRatings(ratings) {
-  return ratings.length >= 5 && ratings.slice(0, 5)
+  if (ratings.length < 5) return false;
+
+  const firstFive: string[] = [];
+
+  for (let i = 0; i < 5; i++) {
+    firstFive.push(ratings[i]);
+  }
+  
+  return firstFive;
 }
 
 function sumFirstFiveRatings(ratings) {
-  const ratingsBool = !!ratings
+  const ratingsBool = Boolean(ratings)
 
   if (ratingsBool) {
     const firstFiveRatings = getFirstFiveRatings(ratings)
@@ -13,10 +21,10 @@ function sumFirstFiveRatings(ratings) {
     let ratingsSum = 0;
 
     for (const rating of firstFiveRatings) {
-      ratingsSum += +rating
+      ratingsSum += Number(rating)
     }
 
-    return { ratingsSum, created_at: +new Date() }
+    return { ratingsSum, created_at: Number(new Date()) }
   }
 
   return { error: 'ratings is required' }

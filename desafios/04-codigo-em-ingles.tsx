9abbb51d08ce84f1cd939306681e0b901c93f421
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-06-15 15:24:04 -0300

    refactor: rename variables for consistency and clarity.

--- desafios/04-codigo-em-ingles.tsx
+++ desafios/04-codigo-em-ingles.tsx
@@ -1,43 +1,46 @@
-// Código em inglês 
-import { useState } from "react"
+// Código em inglês
+import { useState } from "react";
 
 interface Product {
   title: string;
   price: string;
 }
 
-const productList = [
+const productsList = [
   {
-    title: 'Macarrão',
-    price: 'R$ 25,00'
+    title: "Macarrão",
+    price: "R$ 25,00",
   },
   {
-    title: 'Hamburger',
-    price: 'R$ 30,00'
-  }
-]
+    title: "Hamburger",
+    price: "R$ 30,00",
+  },
+];
 
-export function ListProduto() {
-  const [filteredProdutos, setFilteredProdutos] = useState<Product[]>([])
+export function ProductsList() {
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
 
-  function searchProduto(search: string) {
-    const filtrado = productList.filter(product => product.title.includes(search))
+  function searchProduct(search: string) {
+    const filteredProductsList = productsList.filter((product) =>
+      product.title.includes(search)
+    );
 
-    setFilteredProdutos(filtrado)
+    setFilteredProducts(filteredProductsList);
   }
 
   return (
     <div>
-      <input type="text" onChange={(e) => searchProduto(e.target.value)} />
+      <input
+        type="text"
+        onChange={(event) => searchProduct(event.target.value)}
+      />
 
-      {filteredProdutos.map(produto => (
+      {filteredProducts.map((product) => (
         <div>
-          <p>{produto.title}</p>
-          <p>{produto.price}</p>
+          <p>{product.title}</p>
+          <p>{product.price}</p>
         </div>
       ))}
     </div>
-  )
+  );
 }
-
-

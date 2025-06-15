a750a18dc0d2f4436a9f2fcf974f3003780cda81
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-06-15 09:06:03 -0300

    refactor: rename variables for clarity and consistency.

--- desafios/01-nomenclatura-de-variaveis.ts
+++ desafios/01-nomenclatura-de-variaveis.ts
@@ -1,55 +1,55 @@
 // Nomenclatura de variáveis
 
-const list = [
+const userCategories = [
   {
     title: 'User',
-    followers: 5
+    minimumFollowers: 5
   },
   {
     title: 'Friendly',
-    followers: 50,
+    minimumFollowers: 50,
   },
   {
     title: 'Famous',
-    followers: 500,
+    minimumFollowers: 500,
   },
   {
     title: 'Super Star',
-    followers: 1000,
+    minimumFollowers: 1000,
   },
 ]
 
-export default async function getData(req, res) {
-  const github = String(req.query.username)
+export default async function getUserData(req, res) {
+  const username = String(req.query.username)
 
-  if (!github) {
+  if (!username) {
     return res.status(400).json({
       message: `Please provide an username to search on the github API`
     })
   }
 
-  const response = await fetch(`https://api.github.com/users/${github}`);
+  const response = await fetch(`https://api.github.com/users/${username}`);
 
   if (response.status === 404) {
     return res.status(400).json({
-      message: `User with username "${github}" not found`
+      message: `User with username "${username}" not found`
     })
   }
 
-  const data = await response.json()
+  const userData = await response.json()
 
-  const orderList = list.sort((a, b) =>  b.followers - a.followers); 
+  const orderedCategories = userCategories.sort((categoryA, categoryB) =>  categoryB.minimumFollowers - categoryA.minimumFollowers); 
 
-  const category = orderList.find(i => data.followers > i.followers)
+  const category = orderedCategories.find(category => userData.minimumFollowers > category.minimumFollowers)
 
   const result = {
-    github,
-    category: category.title
+    username,
+    category: category?.title || ""
   }
 
   return result
 }
 
-getData({ query: {
+getUserData({ query: {
   username: 'josepholiveira'
 }}, {})
